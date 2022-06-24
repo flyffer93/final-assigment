5299446ca4c521b2b2cb1df1ec93577fd7c75fc7
Thomas Aspelund <cy224@rikstv.no>
2022-06-24 22:24:36 +0200

    changes

--- src/utils/API/index.js
+++ src/utils/API/index.js
@@ -1,5 +1,5 @@
 export const isUserInDatabase = async (user) => {
-    const USER_URL = "https://translaturapp.herokuapp.com/users?name=" + user;
+    const USER_URL = "https://ta-noroff-api.herokuapp.com/users?name=" + user;
     const response = await fetch(USER_URL);
     const names = await response.json();
     if(names.length === 0){          
@@ -9,7 +9,7 @@ export const isUserInDatabase = async (user) => {
 }
 
 export const addUser = async (name) => {
-    await fetch('https://translaturapp.herokuapp.com/users', {
+    await fetch('https://ta-noroff-api.herokuapp.com/users', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -21,7 +21,7 @@ export const addUser = async (name) => {
 }
 
 export const addTranslation = async (body) => {
-    await fetch('https://translaturapp.herokuapp.com/translation', {
+    await fetch('https://ta-noroff-api.herokuapp.com/translations', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -32,7 +32,7 @@ export const addTranslation = async (body) => {
 
 export const deleteTranslation = async (translation) => {
     translation.status = "inactive";
-    const response = await fetch("https://translaturapp.herokuapp.com/translation" + translation.id, {
+    const response = await fetch("https://ta-noroff-api.herokuapp.com/translations" + translation.id, {
         method: 'PATCH',
         headers: {
             'Content-type': 'application/json'
@@ -44,7 +44,7 @@ export const deleteTranslation = async (translation) => {
 }
 
 export const getUsersMostRecentTranslations = async (user) => {
-    const response = await fetch("https://translaturapp.herokuapp.com/translation?_sort=id&_order=desc&_limit=10&status=active&author=" + user)
+    const response = await fetch("https://ta-noroff-api.herokuapp.com/translations?_sort=id&_order=desc&_limit=10&status=active&author=" + user)
     const data = await response.json();
     return data;
 }
